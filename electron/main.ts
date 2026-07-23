3d05a6a90b6766d7f81934da045ed15c0f75a940
Tugraeker <tugraev2@gmail.com>
2026-07-23 12:10:28 +0300

    fix: app-update.yml + backgroundThrottling:false

--- electron/main.ts
+++ electron/main.ts
@@ -133,6 +133,7 @@ function createWindow() {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
       nodeIntegration: false,
+      backgroundThrottling: false,
     },
     backgroundColor: '#121216',
     show: false,
